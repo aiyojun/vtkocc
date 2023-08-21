4e35391998c0d6c9e51d64912678f2e963e58f86
DJun <32353521+aiyojun@users.noreply.github.com>
2023-08-21 17:04:42 +0800

    Add files via upload

--- libshare/QtTools.cpp
+++ libshare/QtTools.cpp
@@ -33,5 +33,30 @@ QString QtTools::readFile(const QString &path) {
     return r;
 }
 
+int QtTools::calc(QWidget* p, const std::string& str, bool isVertical) {
+    std::string xStr = std::regex_replace(str, std::regex(" "), "");
+    double r;
+    if (std::regex_match(xStr, std::regex("^[0-9]+px$"))) {
+        r = std::stoi(xStr.substr(0, xStr.length() - 2));
+    } else if (std::regex_match(xStr, std::regex("^[0-9]+%$"))) {
+        r = (isVertical ? p->width() : p->height()) * std::stod(xStr.substr(0, xStr.length() - 1)) * 0.01;
+    } else if (std::regex_match(xStr, std::regex("^calc\\([0-9]+%([+-][0-9]+px)*\\)$"))) {
+        size_t pos0 = xStr.find('%');
+        r = (isVertical ? p->width() : p->height()) * std::stoi(xStr.substr(5, pos0 - 5)) * 0.01 ;
+        std::string rest = xStr.substr(pos0 + 1, xStr.length() - pos0 - 2);
+        while (rest.length() > 0) {
+            size_t pos = rest.find("px");
+            if (pos == std::string::npos)
+                break;
+            r += std::stoi(rest.substr(0, pos));
+            rest = rest.substr(pos + 2);
+        }
+    } else if (std::regex_match(xStr, std::regex("^[0]+$"))) {
+        r = 0;
+    } else
+        throw std::runtime_error("error distance unit : " + xStr);
+    return (int) r;
+}
+
 
 
--- libshare/QtTools.h
+++ libshare/QtTools.h
@@ -12,11 +12,17 @@ public:
     static QString readFile(const QString& path);
     static inline std::string to_string(const QString& s)
     { return s.toStdString(); }
-
-//    static inline void setColor(QWidget* p, const std::string& color)
-//    { p->setStyleSheet(QString("color: ").append(color.c_str()).append(";")); }
-//    static inline void setBackgroundColor(QWidget* p, const std::string& color)
-//    { p->setStyleSheet(QString("background-color: ").append(color.c_str()).append(";")); }
+    static int calc(QWidget* p, const std::string& str, bool isVertical = false);
+    /**
+     * @deprecated
+     */
+    static inline void setColor(QWidget* p, const std::string& color)
+    { p->setStyleSheet(QString("color: ").append(color.c_str()).append(";")); }
+    /**
+     * @deprecated
+     */
+    static inline void setBackgroundColor(QWidget* p, const std::string& color)
+    { p->setStyleSheet(QString("background-color: ").append(color.c_str()).append(";")); }
     static inline void setBackgroundImage(QWidget* p, const std::string& path) {
         p->setStyleSheet(QString("")
             .append("background: url(").append(path.c_str()).append("); ")
