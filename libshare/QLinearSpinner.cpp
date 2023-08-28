d14ce2c3b04831d340ae8ffe93026d4cf5d4f0a9
jun.dai <1608450902@qq.com>
2023-08-28 09:28:14 +0800

    make QLinearSpinner standard

--- libshare/QLinearSpinner.cpp
+++ libshare/QLinearSpinner.cpp
@@ -8,54 +8,47 @@
 QRect computeCircle(int x, int y, int r)
 { return {x - r, y - r, r * 2, r * 2}; }
 
-QSpinnerAnimation::QSpinnerAnimation(QLinearSpinner* spinner) : QAbstractAnimation(spinner), _spinner(spinner) {
+QRepaintAnimation::QRepaintAnimation(QWidget* companion)
+: QAbstractAnimation(companion), _companion(companion), _duration(3000) {
 
 }
 
-int QSpinnerAnimation::duration() const {
-    return 3000;
+int QRepaintAnimation::duration() const {
+    return _duration;
 }
 
-void QSpinnerAnimation::updateCurrentTime(int currentTime) {
-    _spinner->repaint();
+void QRepaintAnimation::updateCurrentTime(int currentTime) {
+    _companion->repaint();
 }
 
 // --------------
 // QLinearSpinner
 // --------------
 QLinearSpinner::QLinearSpinner(QWidget *parent): QWidget(parent) {
-    _animation = new QSpinnerAnimation(this);
+    _animation = new QRepaintAnimation(this);
     _animation->setLoopCount(-1);
     _animation->start();
 }
 
 void QLinearSpinner::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
-    int w = width();
-    int h = height();
-    int w_2 = int(0.5 * w);
-    int h_2 = int(0.5 * h);
-    int dis = 50;
-    int d_45 = (int) (dis * qCos(M_PI * 0.25));
-
+    int w_2  = (int) (0.5 * width());
+    int h_2  = (int) (0.5 * height());
+    int d_45 = (int) (_spinnerDistance * qCos(M_PI * 0.25));
     double ratio = (double) _animation->currentTime() / _animation->duration() * 2 * M_PI;
-    qDebug() << "iTime : " << _animation->currentTime();
-
     painter.setRenderHints(QPainter::Antialiasing | QPainter::SmoothPixmapTransform);
-    painter.setBrush(QBrush(QColor("#E0C3FC")));
+    painter.setBrush(QBrush(QColor(_spinnerColor)));
     painter.setPen(Qt::PenStyle::NoPen);
     if (!painter.isActive())
         painter.begin(this);
-
-    painter.drawEllipse(computeCircle(w_2, h_2 + dis, int(_spinnerRadius * qCos(ratio))));
+    painter.drawEllipse(computeCircle(w_2, h_2 + _spinnerDistance, int(_spinnerRadius * qCos(ratio))));
     painter.drawEllipse(computeCircle(w_2 + d_45, h_2 + d_45, int(_spinnerRadius * qCos(M_PI * 0.125 + ratio))));
-    painter.drawEllipse(computeCircle(w_2 + dis, h_2, int(_spinnerRadius * qCos(M_PI * 0.25 + ratio))));
+    painter.drawEllipse(computeCircle(w_2 + _spinnerDistance, h_2, int(_spinnerRadius * qCos(M_PI * 0.25 + ratio))));
     painter.drawEllipse(computeCircle(w_2 + d_45, h_2 + -d_45, int(_spinnerRadius * qCos(M_PI * 0.375 + ratio))));
-    painter.drawEllipse(computeCircle(w_2, h_2 + -dis, int(_spinnerRadius * qCos(M_PI * 0.5 + ratio))));
+    painter.drawEllipse(computeCircle(w_2, h_2 + -_spinnerDistance, int(_spinnerRadius * qCos(M_PI * 0.5 + ratio))));
     painter.drawEllipse(computeCircle(w_2 + -d_45, h_2 + -d_45, int(_spinnerRadius * qCos(M_PI * 0.625 + ratio))));
-    painter.drawEllipse(computeCircle(w_2 + -dis, h_2, int(_spinnerRadius * qCos(M_PI * 0.75 + ratio))));
+    painter.drawEllipse(computeCircle(w_2 + -_spinnerDistance, h_2, int(_spinnerRadius * qCos(M_PI * 0.75 + ratio))));
     painter.drawEllipse(computeCircle(w_2 + -d_45, h_2 + d_45, int(_spinnerRadius * qCos(M_PI * 0.875 + ratio))));
-
     painter.end();
 }
 
--- libshare/QLinearSpinner.h
+++ libshare/QLinearSpinner.h
@@ -4,27 +4,29 @@
 #include <QtWidgets/QWidget>
 #include <QtCore/QAbstractAnimation>
 
-class QLinearSpinner;
-class QSpinnerAnimation;
-
-class QSpinnerAnimation : public QAbstractAnimation {
+class QRepaintAnimation : public QAbstractAnimation {
     Q_OBJECT
+    Q_PROPERTY(int duration READ duration WRITE setDuration)
 
 public:
-    explicit QSpinnerAnimation(QLinearSpinner* spinner);
-    ~QSpinnerAnimation() override = default;
+    explicit QRepaintAnimation(QWidget* companion);
+    ~QRepaintAnimation() override = default;
     int duration() const override;
+    void setDuration(int d) { _duration = d; }
 
 protected:
     void updateCurrentTime(int currentTime) override;
 
 private:
-    QLinearSpinner* _spinner;
+    int _duration;
+    QWidget* _companion;
 };
 
 class QLinearSpinner : public QWidget {
     Q_OBJECT
     Q_PROPERTY(int spinnerRadius READ spinnerRadius WRITE setSpinnerRadius)
+    Q_PROPERTY(int spinnerDistance READ spinnerDistance WRITE setSpinnerDistance)
+    Q_PROPERTY(int spinnerColor READ spinnerColor WRITE setSpinnerColor)
 
 public:
     explicit QLinearSpinner(QWidget *parent = nullptr);
@@ -32,14 +34,19 @@ public:
 
     int spinnerRadius() const { return _spinnerRadius; }
     void setSpinnerRadius(int r) { _spinnerRadius = r; }
+    int spinnerDistance() const { return _spinnerDistance; }
+    void setSpinnerDistance(int d) { _spinnerDistance = d; }
+    QString spinnerColor() const { return _spinnerColor; }
+    void setSpinnerColor(const QString& c) { _spinnerColor = c; }
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    QSpinnerAnimation* _animation;
-    QVector<QRect> rects;
-    int _spinnerRadius = 12;
+    int _spinnerRadius    = 12;
+    int _spinnerDistance  = 50;
+    QString _spinnerColor = "#E0C3FC";
+    QRepaintAnimation* _animation;
 };
 
 
